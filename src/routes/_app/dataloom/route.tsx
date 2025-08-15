e089eda6ca2b244643c386e880c26d96bfa1bccd
Marno van Niekerk <marnovn071@gmail.com>
2025-08-15 11:59:18 +0200

    refactor: remove unused DATALOOM_URL and beforeLoad logic from Dataloom route

--- src/routes/_app/dataloom/route.tsx
+++ src/routes/_app/dataloom/route.tsx
@@ -1,15 +1,14 @@
 import { DataloomLayoutScreen } from "@/components/app/dataloom/dataloom.screen";
-import { DATALOOM_URL } from "@/domain/http/api";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/dataloom")({
     component: () => <DataloomLayoutScreen />,
-    beforeLoad: () => {
-        if (DATALOOM_URL === "GL_DATALOOM_URL") {
-            throw redirect({
-                to: "/",
-                search: { page: 1 },
-            });
-        }
-    },
+    // beforeLoad: () => {
+    //     if (DATALOOM_URL === "GL_DATALOOM_URL") {
+    //         throw redirect({
+    //             to: "/",
+    //             search: { page: 1 },
+    //         });
+    //     }
+    // },
 });
