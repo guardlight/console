ebfc5979886492beba6234961a6e43450f8c7ba0
Marno van Niekerk <marnovn071@gmail.com>
2025-08-15 09:31:35 +0200

    feat(dataloom): add redirect logic in route beforeLoad for invalid Dataloom URL

--- src/routes/_app/dataloom/route.tsx
+++ src/routes/_app/dataloom/route.tsx
@@ -1,6 +1,14 @@
 import { DataloomLayoutScreen } from "@/components/app/dataloom/dataloom.screen";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/dataloom")({
     component: () => <DataloomLayoutScreen />,
+    beforeLoad: () => {
+        if (import.meta.env.VITE_DATALOOM_URL === "GL_DATALOOM_URL") {
+            throw redirect({
+                to: "/",
+                search: { page: 1 },
+            });
+        }
+    },
 });
